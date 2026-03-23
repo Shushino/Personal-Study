5c8949b2cfbdf543020af7f6bbab31b98d037514
Shushino <georgeomowu@gmail.com>
2026-03-23 21:49:50 +0100

    Add timer project files

---
+++ timer.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    int h, m, s, a, err;
+    err = a = 0;
+
+    cout << "24 Hour Digital Timer" << endl;
+    while (err == 0)
+    {
+        cout << "Enter hour: " << endl;
+        cin >> h;
+        cout << "Enter minutes: " << endl;
+        cin >> m;
+        cout << "Enter seconds: " << endl;
+        cin >> s;
+
+        (h < 24 && m < 60 && s < 60) ? err++ : system("cls");
+    }
+    while (a == 0)
+    {
+        system("cls");
+        cout << h << ":" << m << ":" << s << endl;
+        Sleep(1000);
+        s++;
+
+        if (s > 59)
+        {
+            s = 0;
+            m++;
+        }
+        if (m > 59)
+        {
+            m = 0;
+            h++;
+        }
+        if (h > 23)
+        {
+            h = 0;
+        }
+    }
+    return 0;
+}
