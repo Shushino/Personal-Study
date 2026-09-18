89fbea16d15ed3c07ccca9f8804fbf5c7abea46f
George Omowu <georgeomowu@gmail.com>
2026-09-18 21:21:57 +0100

    Day 16.

--- Full-Stack/index.js
+++ Full-Stack/index.js
@@ -1,8 +1,20 @@
-let age = 22
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ("")
 
-if (age < 21){
-    console.log("You can not enter the club!")
-}
-else{
-    console.log("Welcome!")
+function startGame()
+{
+    if (sum <= 20) {
+        message = ("Do you want to draw a new card?")
+    }else if (sum === 21) {
+        message = ("Wohoo! You've got Blackjack!")
+    hasBlackJack = true
+    } else {
+    message = ("You're out of the game!")
+    isAlive = false
+    }
+    console.log(message)
 }
