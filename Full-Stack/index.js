c10dc874b270a11df54c72b189b8da195896e08c
George Omowu <georgeomowu@gmail.com>
2026-09-19 13:33:33 +0100

    Day 17.

--- Full-Stack/index.js
+++ Full-Stack/index.js
@@ -1,20 +1,38 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
+let thirdCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ("")
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
 
 function startGame()
+{
+    renderGame()
+}
+function renderGame()
 {
     if (sum <= 20) {
         message = ("Do you want to draw a new card?")
     }else if (sum === 21) {
-        message = ("Wohoo! You've got Blackjack!")
+        message = ("You've got Blackjack!")
     hasBlackJack = true
     } else {
     message = ("You're out of the game!")
     isAlive = false
     }
-    console.log(message)
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
+}
+
+function newCard()
+{
+    sum += thirdCard
+    startGame()
+
 }
