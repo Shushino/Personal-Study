a208766338a2a04a1f5ed30018364997fd615c35
George Omowu <georgeomowu@gmail.com>
2026-09-23 23:52:24 +0100

    Day 21.

--- Full-Stack/test.js
+++ Full-Stack/test.js
@@ -1,12 +1,26 @@
-let homeScore = 0
-let guestScore = 0
+// let homeScore = 0
+// let guestScore = 0
 
-function addHomePoints(points) {
-    homeScore += points
-    document.getElementById("home-score").textContent = homeScore
-}
+// function addHomePoints(points) {
+//     homeScore += points
+//     document.getElementById("home-score").textContent = homeScore
+// }
 
-function addGuestPoints(points) {
-    guestScore += points
-    document.getElementById("guest-score").textContent = guestScore
+// function addGuestPoints(points) {
+//     guestScore += points
+//     document.getElementById("guest-score").textContent = guestScore
+// }
+
+let age = 2026
+
+if (age <= 5) {
+    console.log("Free!")
+} else if (age >= 6 && age < 17) {
+    console.log("Child Discount")
+} else if (age >= 18 && age < 26) {
+    console.log("Student discount")
+} else if (age >= 27 && age < 66) {
+    console.log("Full price")
+} else {
+    console.log("Senior Citizen discount")
 }
