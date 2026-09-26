dc56a82edf4c690a41e8c1a021e3185ec8686549
George Omowu <georgeomowu@gmail.com>
2026-09-26 14:07:27 +0100

    Day 23.

--- Full-Stack/index.js
+++ Full-Stack/index.js
@@ -1,60 +1,19 @@
-let player = {
-    name: "Shushino",
-    chips: 150
-}
-let cards = []
-let sum = 0
-let hasBlackJack = false
-let isAlive = true
-let message = ("")
-let messageEl = document.getElementById("message-el")
-let sumEl = document.querySelector("#sum-el")
-let cardsEl = document.querySelector("#cards-el")
-let playerEl = document.getElementById("player-el")
-playerEl.textContent = player.name + ": $" + player.chips
+let myLeads = []
 
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-function getRandomCard(){
-    let randomNumber = Math.floor(Math.random() * 13) + 1
-    if (randomNumber === 1) {
-        return 11
-    } else if (randomNumber >= 11) {
-        return 10
-    }
-    return randomNumber
-}
-
-function startGame()
+inputBtn.addEventListener("click", function()
 {
-    cards = [getRandomCard(), getRandomCard()]
-    sum = cards[0] + cards[1]
-    hasBlackJack = false
-    isAlive = true
-    renderGame()
-}
+    myLeads.push(inputEl.value)
+})
 
-function renderGame()
-{
-    if (sum <= 20) {
-        message = ("Do you want to draw a new card?")
-    } else if (sum === 21) {
-        message = ("You've got Blackjack!")
-        hasBlackJack = true
-    } else {
-        message = ("You're out of the game!")
-        isAlive = false
-    }
-    messageEl.textContent = message
-    sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + cards.join(" ")
-}
+let listItems = ""
 
-function newCard()
+for (let i = 0; i < myLeads.length; i++)
 {
-    if (isAlive && !hasBlackJack) {
-        let card = getRandomCard()
-        cards.push(card)
-        sum += card
-        renderGame()
-    }
+    listItems += "<li>" + myLeads[i] + "</li> "
+
 }
+ulEl.innerHTML = listItems
